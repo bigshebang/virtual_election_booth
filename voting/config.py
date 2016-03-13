5f26c902d04e50f5d9f3cfbebc84f3d12d98bcf7
electionCentral <electionCentral@csci455.com>
2016-03-13 19:55:45 -0400

    add config variables for flask-mysqldb

--- voting/config.py
+++ voting/config.py
@@ -12,11 +12,16 @@ with open('.voting_secret_key', 'a+') as secret:
 ##### SERVER SETTINGS #####
 SECRET_KEY = key
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///ctfd.db'
-#SQLALCHEMY_DATABASE_URI = 'mysql://voting-dba:password@localhost/voting'
+#SQLALCHEMY_DATABASE_URI = 'mysql://ec-dba:mysql:Password!@localhost/ElectionCentral'
+MYSQL_HOST = "localhost"
+MYSQL_USER = "ec-dba"
+MYSQL_PASSWORD = "mysql:Password!"
+MYSQL_DB = "ElectionCentral"
 SESSION_TYPE = "filesystem"
 SESSION_FILE_DIR = "/tmp/flask_session"
 SESSION_COOKIE_HTTPONLY = True
-PERMANENT_SESSION_LIFETIME = 604800 # 7 days in seconds
+PERMANENT_SESSION_LIFETIME = 86400 # 1 day in seconds
+#PERMANENT_SESSION_LIFETIME = 604800 # 7 days in seconds
 HOST = "localhost"
-UPLOAD_FOLDER = os.path.normpath('static/uploads')
+#UPLOAD_FOLDER = os.path.normpath('static/uploads')
 TRUSTED_PROXIES = ['^127\.0\.0\.1$']
