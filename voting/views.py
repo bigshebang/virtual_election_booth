b816ae5a793a6be10fddb7bf05993921ddb8f54f
electionCentral <electionCentral@csci455.com>
2016-04-03 14:45:51 -0400

    add a couple updates to be ready for full front end integration

--- voting/views.py
+++ voting/views.py
@@ -7,12 +7,20 @@ def home():
 	if request.method == "GET":
 		return render_template("index.html")
 
+@views.route("/election", methods=["GET"])
+def election_page():
+	return render_template("election.html", results=[("", 5)])
+
 @views.route("/vote", methods=["GET", "POST"])
 def vote_page():
 	if request.method == "GET":
-		return render_template("vote.html")
+		return render_template("vote.html", listLen=0, candidates=[""])
 	elif request.method == "POST":
+		#user voted, now we need to process the data
 		return render_template("vote.html")
 	else: #weird HTTP method we don't support
 		return render_template("index.html")
+
+def vote():
+	return ""
 
