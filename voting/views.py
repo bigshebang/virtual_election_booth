aa18022137bfce3ccca9d45d969dc96feb756739
bigshebang <ljm3103@rit.edu>
2016-04-04 13:44:25 -0400

    fix flow of code when visitig pages

--- voting/views.py
+++ voting/views.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request, redirect, abort, session, Blueprint
-from voting.utils import loggedIn
+from voting.utils import loggedIn, getCurElection
 
 views = Blueprint('views', __name__)
 
 @views.route("/", methods=["GET"])
 def home():
-	if request.method == "GET":
-		return render_template("index.html")
+	curElection = getCurElection()
+	return render_template("index.html", logged_in=loggedIn(), show_results=curElection)
 
 @views.route("/election", methods=["GET"])
 def election_page():
+	if not loggedIn(): #not logged in, make em register
+		return redirect("/register")
+
 	curElection = getCurElection()
 	if curElection:
 		candidates = getCandidates(curElection) #get candidates in election
@@ -20,29 +23,33 @@ def election_page():
 			votes.append(getCandidateVotes(c))
 
 		voted, notVoted = getVoters(curElection)
-		return render_template("election.html", results=[candidates, votes], voted=voted, notVoted=notVoted)
+		return render_template("election.html", logged_in=True, show_results=True, results=[candidates, votes], voted=voted, notVoted=notVoted)
 	else: #no election today, return to home page
 		return redirect("/")
 
 @views.route("/vote", methods=["GET", "POST"])
 def vote_page():
+	if not loggedIn(): #not logged in, make em register
+		return redirect("/register")
+
 	if request.method == "GET":
 		curElection = getCurElection()
-		candidates = getCandidates(curElection)
-		return render_template("vote.html", listLen=len(candidates), candidates=candidates)
+		if curElection:
+			candidates = getCandidates(curElection)
+			return render_template("vote.html", logged_in=True, show_results=True, listLen=len(candidates), candidates=candidates)
 	elif request.method == "POST":
 		#user voted, now we need to process the data
 		curElection = getCurElection()
-		candidates = getCandidates(curElection)
+		if curElection:
+			candidates = getCandidates(curElection)
 
-		return render_template("vote.html", voted=True)
+			return render_template("vote.html", logged_in=True, voted=True, show_results=True)
+
+	#there is no election today
+	return render_template("vote.html", logged_in=True, show_results=False)
 
 #perform the vote by updating database
 def vote():
-	return ""
-
-#get the current active election for the day
-def getCurElection():
 	return ""
 
 #return a list of the candidates running in the given election
