ec15ee259f40b4303da4172d8f9a1f7b519838c9
bigshebang <ljm3103@rit.edu>
2016-04-05 21:21:46 -0400

    fix up the vote code and functions

--- voting/views.py
+++ voting/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, abort, session, Blueprint
-from voting.utils import loggedIn, getCurElection
+from voting.utils import loggedIn, getCurElection, tryLogin
 
 views = Blueprint('views', __name__)
 
@@ -59,21 +59,36 @@ def vote_page():
 			return render_template("vote.html", logged_in=True, show_results=True,
 									listLen=len(candidates), candidates=candidates)
 	elif request.method == "POST":
-		#user voted, now we need to process the data
+		#user voted, now we need to process the data if there's an election today
 		curElection = getCurElection()
+
+		#when this if statement is true, the election being voted in today is valid
 		if curElection:
 			candidates = getCandidates(curElection)
 
-			candidate = request.form["candidate"][-1] #get the candidate temp ID
-
-			#if candidate id is out of bounds for this election then this is a malicious voting
-			#attempt. don't count the vote, but record the invalid vote.
-			if candidate >= len(candidates):
-				vote(curElection, voted=False)
+			error = None
+			result = False
+			#user should also put their password in to vote
+			if not tryLogin(session["username"], request.form["password"]):
+				error = "Invalid password."
+			elif not validCandidateID(curElection, request.form["candidate"]):
+				error = "Invalid candidate ID given. Voter fraud detected - not counting vote."
 			else:
-				vote(curElection, candidate=candidates[candidate])
+				#if candidate id is out of bounds for this election then this is a malicious voting
+				#attempt. don't count the vote, but record the invalid vote.
+				if candidate >= len(candidates):
+					vote(curElection, voted=False)
+				else:
+					candidate = request.form["candidate"][-1] #get the candidate temp ID
+					vote(curElection, candidate=candidates[candidate])
 
-			return render_template("vote.html", logged_in=True, voted=True, show_results=True)
+			if result: #vote is valid
+				return render_template("vote.html", logged_in=True, voted=True, show_results=True)
+			else: #vote is invalid
+				if not error:
+					error = "There was a problem with your vote. Please try again."
+
+				return render_template("vote.html", logged_in=True, error=error, show_results=True)
 
 	#there is no election today
 	return render_template("vote.html", logged_in=True, show_results=False)
@@ -83,10 +98,11 @@ def vote(election, candidate=None, voted=True):
 	#when we create an election, we need to create the corresponding rows in electionData
 	#because this function will assume they're just there
 
+	#shouldn't actually need this
 	#verify that the election is still active
-	curTime = getCurTime()
-	if not electionActive(election, curTime):
-		return False
+	# curTime = getCurTime()
+	# if not electionActive(election, curTime):
+	# 	return False
 
 	#update mysql db
 	#WE NEED to use a mutex or lock so that only one process can ever perform this update on the db
@@ -95,7 +111,7 @@ def vote(election, candidate=None, voted=True):
 		#to the voterHistory table with the proper data
 		pass
 	else: #failed vote
-		#add the
+		#add the vote to voterHistory table but set the voted value to false
 		pass
 
 	return False
@@ -108,7 +124,12 @@ def validElectionID(num):
 
 	return False
 
+#make sure candidate ID given for this election is valid
+def validCandidateID(election, candidate):
+	return True
+
 #given an election and current time, see if that election is still active
+#WE MAY NOT NEED THIS
 def electionActive(election, curTime):
 	return True
 
