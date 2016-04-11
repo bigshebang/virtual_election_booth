0327d15eae4d61bb3612c801359e651cd0c29727
bigshebang <ljm3103@rit.edu>
2016-04-11 03:29:23 -0400

    fix age validation

--- voting/utils.py
+++ voting/utils.py
@@ -70,7 +70,7 @@ def validAge(dob):
 
 	#if date cannot be converted it isn't a real date
 	try:
-		dobDate = datetime.date(vals[0], vals[1], vals[2])
+		dobDate = datetime.date(int(vals[0]), int(vals[1]), int(vals[2]))
 	except:
 		return False
 
